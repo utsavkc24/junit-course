58218c245dbc82cc19f56b2fc8acd6572861539b
Utsav Kumar <utsavkc24@gmail.com>
2022-01-28 14:04:53 +0530

    Common methods like - assertEquals, assertTrue and assertFalse

--- src/test/java/com/example/in28minutes/helper/StringHelperTest.java
+++ src/test/java/com/example/in28minutes/helper/StringHelperTest.java
@@ -1,6 +1,8 @@
 package com.example.in28minutes.helper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +14,16 @@ public class StringHelperTest {
     public void testTruncateAInFirst2Positions() {
         assertEquals(stringHelper.truncateAInFirst2Positions("AACD"), "CD"); // assertMethod() test expected value with
                                                                              // acutal value.
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_NegativeScenario() {
+        assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("ABABC"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_PositiveScenario() {
+        assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
     }
 
 }
