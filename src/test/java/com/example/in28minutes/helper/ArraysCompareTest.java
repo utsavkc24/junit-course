2c94146d2f5f33ac5087ff633b237fe9d0104e57
Utsav Kumar <utsavkc24@gmail.com>
2022-01-28 14:57:27 +0530

    Timeout

--- src/test/java/com/example/in28minutes/helper/ArraysCompareTest.java
+++ src/test/java/com/example/in28minutes/helper/ArraysCompareTest.java
@@ -4,8 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 public class ArraysCompareTest {
 
@@ -23,5 +26,16 @@ public class ArraysCompareTest {
     public void testArraySort_NullArray() {
         int[] number = null;
         assertThrows(NullPointerException.class, () -> Arrays.sort(number));
+    }
+
+    // This method test whether this loop run under a alloted time(Using @Timeout)
+    @Test
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    public void testSort_Performance() {
+        int[] array = { 1, 2, 3, 5, 7 };
+        for (int i = 0; i < 100000; i++) {
+            array[0] = (int) new Random().nextInt(1000);
+            Arrays.sort(array);
+        }
     }
 }
